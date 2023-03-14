50b770028a4b3081ec6a6823dc3ff33a9c4136d4
tam.hoang <tam.hoang@napaglobal.com>
2023-03-14 11:12:08 +0700

    component line chart

--- src/pages/Chart/index.js
+++ src/pages/Chart/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import {View, StyleSheet, Text, Dimensions} from 'react-native';
+import {View, StyleSheet, Text, Dimensions, Pressable} from 'react-native';
 import {COLORS} from '../../util/colors';
 import {LineChart} from 'react-native-chart-kit';
-import {LinearGradient} from 'react-native-linear-gradient';
+import ChartClockSVG from '../../assets/icons/chart_clock.svg';
+import NoDataSVG from '../../assets/icons/no_data_chart.svg';
+import PrevSVG from '../../assets/icons/ic_chart_prev.svg';
+import NextSVG from '../../assets/icons/ic_chart_next.svg';
 
 const ChartScreen = () => {
   const data = {
@@ -22,9 +25,11 @@ const ChartScreen = () => {
     ],
     datasets: [
       {
-        data: [48, 152, 147, 230, 180, 300, 77, 270, 188, 260, 233, 145],
+        data: [[]],
+        // data: [48, 152, 147, 230, 180, 300, 77, 270, 188, 260, 233, 145],
         strokeWidth: 2,
-        color: () => '#103F37',
+        color: () => 'transparent',
+        // color: () => '#103F37',
       },
     ],
   };
@@ -63,29 +68,80 @@ const ChartScreen = () => {
   };
   return (
     <View style={styles.container}>
-      <Text>Bezier Line Chart</Text>
-      <LineChart
-        data={data}
-        width={Dimensions.get('window').width}
-        height={355}
-        bezier={false}
-        withVerticalLines={false}
-        chartConfig={chartConfiguration}
-        xLabelsOffset={10}
-        yLabelsOffset={20}
-        segments={6}
-        fromZero={true}
-      />
+      <View>
+        <LineChart
+          data={data}
+          width={Dimensions.get('window').width}
+          height={355}
+          bezier={false}
+          withVerticalLines={false}
+          chartConfig={chartConfiguration}
+          xLabelsOffset={10}
+          yLabelsOffset={20}
+          segments={6}
+          fromZero={true}
+          fromNumber={300}
+        />
+        {/* <Pressable style={styles.refresh}>
+          <ChartClockSVG />
+        </Pressable> */}
+        <View style={styles.noData}>
+          <NoDataSVG />
+          <Text style={styles.noDataText}>No data</Text>
+        </View>
+        <View style={styles.year}>
+          <PrevSVG />
+          <Text style={styles.yearText}>2020</Text>
+          <NextSVG />
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: COLORS.gray,
+    // flex: 1,
+    backgroundColor: COLORS.white,
+    paddingBottom: 20,
+    borderRadius: 8,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  refresh: {
+    position: 'absolute',
+    bottom: 65,
+    right: 25,
+  },
+  noData: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    rowGap: 3,
+  },
+  year: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    columnGap: 60,
+    flexDirection: 'row',
+  },
+  yearText: {
+    fontWeight: 700,
+    fontSize: 16,
+    color: '#435C70',
+  },
+  noDataText: {
+    color: '#103F37',
+    fontSize: 20,
+    fontWeight: 700,
   },
   text: {
     fontSize: 24,
